385f7adfff83a39bc27aeca5e79c6463a9862385
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2021-09-27 16:52:08 -0300

    Fifth commit, JavaScript finish

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+const plans = [
+  { pages: "10K ", price: "$8 " },
+  { pages: "50K ", price: "$12 " },
+  { pages: "100K ", price: "$16 " },
+  { pages: "500K ", price: "$24 " },
+  { pages: "1M ", price: "$36 " },
+];
+
+const pages = document.querySelector(".card__pages");
+const prices = document.querySelector(".card__prices");
+const rangeControl = document.querySelector(".card__range");
+
+const refreshPrices = (index) => {
+  prices.firstChild.textContent = plans[index]["price"];
+};
+
+const refreshPages = (index) => {
+  pages.firstChild.textContent = plans[index]["pages"];
+};
+
+rangeControl.addEventListener("change", () => {
+  let percentage = rangeControl.value * 25;
+
+  document.documentElement.style.setProperty(
+    "--Bar-percentage",
+    `${percentage}%`
+  );
+
+  refreshPrices(rangeControl.value);
+  refreshPages(rangeControl.value);
+});
